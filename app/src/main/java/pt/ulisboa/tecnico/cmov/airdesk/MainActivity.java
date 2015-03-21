f2c27b90ab4d1063e7d4d916bc82458c15332b85
erra33 <erikler@kth.se>
2015-03-21 20:10:52 +0000

    Revert "Added some random activities"
    This reverts commit 01fc271e66a70fd5e7881bebf5de414244b6c237.

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/MainActivity.java
@@ -1,11 +1,9 @@
 package pt.ulisboa.tecnico.cmov.airdesk;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -37,11 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    // Called when the user clicks the Send button
-    public void startMyWs(View view) {
-        Intent intent = new Intent(this, MyWorkspaceActivity.class);
-        startActivity(intent);
     }
 }
