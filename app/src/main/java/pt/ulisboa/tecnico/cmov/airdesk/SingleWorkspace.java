01fc271e66a70fd5e7881bebf5de414244b6c237
Daniel Lindström <d.lindstrom@live.com>
2015-03-21 19:09:56 +0000

    Added some random activities

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/SingleWorkspace.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/SingleWorkspace.java
@@ -1,26 +1,24 @@
 package pt.ulisboa.tecnico.cmov.airdesk;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SingleWorkspace extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_single_workspace);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_single_workspace, menu);
         return true;
     }
 
@@ -37,11 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    // Called when the user clicks the Send button
-    public void startMyWs(View view) {
-        Intent intent = new Intent(this, MyWorkspaceActivity.class);
-        startActivity(intent);
     }
 }
