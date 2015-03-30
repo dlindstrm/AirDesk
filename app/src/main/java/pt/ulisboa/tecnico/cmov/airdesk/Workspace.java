f46c837987ed5a8ff75bc31e9ae0a766aaaee7c2
erra33 <erikler@kth.se>
2015-03-30 19:02:08 +0100

    add public

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/Workspace.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/Workspace.java
@@ -7,10 +7,12 @@ public class Workspace {
     // Labels Table Columns names
     public static final String KEY_ID = "id";
     public static final String KEY_title = "title";
+    public static final String KEY_public = "public";
     public static final String KEY_createdAt = "createdAt";
 
     // property help us to keep data
     public int ws_ID;
     public String title;
     public String createdAt;
+    public int Public;
 }
