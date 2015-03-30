1c63db703c265e0b03f4166a294316dc50955cf7
erra33 <erikler@kth.se>
2015-03-31 00:30:50 +0100

    update name

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/Keywords.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/Keywords.java
@@ -3,7 +3,7 @@ package pt.ulisboa.tecnico.cmov.airdesk;
 /**
  * Created by erra on 2015-03-30.
  */
-public class Keyword {
+public class Keywords {
     // Labels table name
     public static final String TABLE = "Keyword";
 
