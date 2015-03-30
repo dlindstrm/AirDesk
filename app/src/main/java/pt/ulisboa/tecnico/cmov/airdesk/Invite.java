4478b8a7eee83c7ab9d62f23105f62d884138747
erra33 <erikler@kth.se>
2015-03-30 23:52:29 +0100

    update

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/Invite.java
@@ -0,0 +1,15 @@
+package pt.ulisboa.tecnico.cmov.airdesk;
+
+/**
+ * Created by erra on 2015-03-30.
+ */
+public class Invite {
+    // Labels table name
+    public static final String TABLE = "Invite";
+
+    // Labels Table Columns names
+    public static final String KEY_ID = "id";
+    public static final String KEY_workspaceID = "workspaceID";
+    public static final String KEY_email = "email";
+
+}
