45e25e94480623f1a4db2380938004b86096eddf
erra33 <erikler@kth.se>
2015-03-30 23:53:10 +0100

    update

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/Keyword.java
@@ -0,0 +1,14 @@
+package pt.ulisboa.tecnico.cmov.airdesk;
+
+/**
+ * Created by erra on 2015-03-30.
+ */
+public class Keyword {
+    // Labels table name
+    public static final String TABLE = "Keyword";
+
+    // Labels Table Columns names
+    public static final String KEY_workspaceID = "workspaceID";
+    public static final String KEY_email = "email";
+
+}
